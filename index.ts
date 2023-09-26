5ca882f283c41b802999fd080ccc31f18acf86f3
narita1980 <238369+narita1980@users.noreply.github.com>
2023-09-26 20:07:54 +0900

    以下を実施 https://bun.sh/docs/quickstart#install-a-package

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
+import figlet from "figlet";
+
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-      return new Response("Bun!");
+        const body = figlet.textSync("Bun!");
+        return new Response(body);
     },
   });
   
