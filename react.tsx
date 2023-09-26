2f46fee495b292f6a4f5cf73744ec40e3b59a34b
narita1980 <238369+narita1980@users.noreply.github.com>
2023-09-26 20:18:49 +0900

    以下を実施 https://bun.sh/docs/runtime/loaders#jsx

---
+++ react.tsx
@@ -0,0 +1,10 @@
+function Component(props: {message: string}) {
+    return (
+      <body>
+        <h1 style={{color: 'red'}}>{props.message}</h1>
+      </body>
+    );
+  }
+  
+  console.log(<Component message="Hello world!" />);
+  
